354c317374089f9475c3806ebf73b915b0ae4fbd
Cory Buecker <email@corybuecker.com>
2025-08-20 23:53:00 +0000

    WIP

--- ina228/src/main.rs
+++ ina228/src/main.rs
@@ -10,18 +10,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     let mut i2c = I2cdev::new("/dev/i2c-1")?;
-    let device_address: u16 = 0x48;
+    let device_address: u16 = 0x40;
 
-    debug!("Reading from device at address: 0x{:02X}", device_address);
-    let mut buffer = [0u8; 2];
-    match i2c.read(device_address, &mut buffer) {
+    match i2c.write(device_address, &[0x80]) {
         Ok(_) => {
-            debug!("Read data: {:02X?}", buffer);
+            let result = i2c.write(device_address, &[0x00, 0x80, 0x00]);
+            debug!("Write result: {:?}", result);
         }
         Err(e) => {
-            debug!("Failed to read from device: {}", e);
+            debug!("Failed to write to device: {}", e);
             return Err(Box::new(e));
         }
     }
+
+    // debug!("Reading from device at address: 0x{:02X}", device_address);
+    // let mut buffer = [0u8; 2];
+    // match i2c.read(device_address, &mut buffer) {
+    //     Ok(_) => {
+    //         debug!("Read data: {:02X?}", buffer);
+    //     }
+    //     Err(e) => {
+    //         debug!("Failed to read from device: {}", e);
+    //         return Err(Box::new(e));
+    //     }
+    // }
     Ok(())
 }
