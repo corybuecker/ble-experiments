37fde359fe9e4d488d65ba2d1f485377bb8d3f40
Cory Buecker <email@corybuecker.com>
2025-08-23 09:21:28 -0500

    --wip-- [skip ci]

--- ina228/src/main.rs
+++ ina228/src/main.rs
@@ -1,38 +1,40 @@
-use embedded_hal::i2c::I2c;
+use anyhow::{Result, anyhow};
+use embedded_hal::i2c::{self, I2c};
 use linux_embedded_hal::I2cdev;
-use std::error::Error;
-use tracing::debug;
+use std::{error::Error, thread::sleep, time::Duration};
+use tracing::{debug, info};
 use tracing_subscriber::layer::SubscriberExt;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let stdout_log = tracing_subscriber::fmt::layer().pretty();
-    let subscriber = tracing_subscriber::registry().with(stdout_log);
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::registry().with(tracing_subscriber::fmt::layer().pretty()),
+    )
+    .expect("setting default subscriber failed");
 
-    let mut i2c = I2cdev::new("/dev/i2c-1")?;
-    let device_address: u16 = 0x40;
+    let i2c = I2cdev::new("/dev/i2c-1")?;
+    let device_address: u8 = 0x40; // A0 and A1 tied to GND, 1000000, R/W low
 
-    match i2c.write(device_address, &[0x80]) {
-        Ok(_) => {
-            let result = i2c.write(device_address, &[0x00, 0x80, 0x00]);
-            debug!("Write result: {:?}", result);
-        }
-        Err(e) => {
-            debug!("Failed to write to device: {}", e);
-            return Err(Box::new(e));
-        }
+    reset_device(i2c, device_address)?;
+
+    Ok(())
+}
+
+fn reset_device(mut i2c: I2cdev, address: u8) -> Result<()> {
+    let reset_register: u8 = 0x00; // 0000 0000
+    let reset_device_command: [u8; 3] = [reset_register, 0x80, 0x00];
+
+    let mut register_buffer = [0u8; 2];
+
+    i2c.write(address, &reset_device_command)?;
+    i2c.write_read(address, &[reset_register], &mut register_buffer)?;
+
+    if register_buffer.eq(&[0x00, 0x00]) == false {
+        return Err(anyhow!("Failed to reset device"));
     }
 
-    // debug!("Reading from device at address: 0x{:02X}", device_address);
-    // let mut buffer = [0u8; 2];
-    // match i2c.read(device_address, &mut buffer) {
-    //     Ok(_) => {
-    //         debug!("Read data: {:02X?}", buffer);
-    //     }
-    //     Err(e) => {
-    //         debug!("Failed to read from device: {}", e);
-    //         return Err(Box::new(e));
-    //     }
-    // }
+    std::thread::sleep(std::time::Duration::from_millis(10));
+
+    info!("Device reset successfully");
+
     Ok(())
 }
