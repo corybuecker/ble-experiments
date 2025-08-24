060cc28680ee6f32b556985a4516f413782e2475
Cory Buecker <email@corybuecker.com>
2025-08-24 09:12:19 -0500

    --wip-- [skip ci]

--- embassy-experiment/src/main.rs
+++ embassy-experiment/src/main.rs
@@ -1,11 +1,14 @@
 #![no_std]
 #![no_main]
+#![macro_use]
+
+use defmt_rtt as _;
+use embassy_nrf as _;
+use panic_probe as _;
 
 use defmt::info;
-use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_nrf::{self as _, config};
-use panic_probe as _; // global logger + panicking behavior
+use embassy_nrf::config;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
